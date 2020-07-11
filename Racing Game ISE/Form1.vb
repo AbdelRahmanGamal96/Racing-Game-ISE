6be5a892e8095ae2ddb473d4269456f7fc63b609
AbdelRahmanGamal96 <abdelrahmangamal225@gmail.com>
2020-07-11 16:37:11 +0700

    Design enemy car , I need to fix bug of score.

--- Racing Game ISE/Form1.vb
+++ Racing Game ISE/Form1.vb
@@ -51,6 +51,12 @@
     End Sub
 
     Private Sub EnemyCar1_Click(sender As Object, e As EventArgs) Handles EnemyCar1.Click
-
+        EnemyCar1.Top += speed
+        If EnemyCar1.Top >= Me.Height Then
+            score += 1
+            Score_Text.Text = "Score " & score
+            EnemyCar1.Top = -(CInt(Math.Ceiling(Rnd() * 150)) + EnemyCar1.Height)
+            EnemyCar1.Left = CInt(Math.Ceiling(Rnd() * 50)) + 0
+        End If
     End Sub
 End Class
